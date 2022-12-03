829f6db7f0f182be3233c84f73e43628f14c2bef
mmolta <mcmolta10@gmail.com>
2022-12-03 01:12:54 -0500

    initial commit - begin adding projects to ref

---
+++ js/ref.js
@@ -0,0 +1,49 @@
+// template
+/*
+{
+    name: '',
+    location: '',
+    duration: '',
+    type: '',
+    cost: '',
+    link: ''
+}
+*/
+
+// "database"
+const ref = {
+    milwaukee: {
+        name: 'Park East Corridor',
+        location: 'Milwaukee, Wisconsin',
+        duration: '',
+        type: '',
+        cost: '',
+        link: 'https://www.milwaukeeindependent.com/syndicated/park-east-corridor-freeway-teardown-helped-put-milwaukee-national-stage-summer/'
+    },
+    rochester: {
+        name: 'Inner Loop Jawn',
+        location: 'Rochester, New York',
+        duration: '',
+        type: '',
+        cost: '',
+        link: ''
+    },
+    portland: {
+        name: 'Harbor Drive',
+        location: 'Portland, Oregon',
+        duration: 'Four years',
+        type: '',
+        cost: '$20 million',
+        link: 'https://www.cnu.org/what-we-do/build-great-places/harbor-drive'
+    },
+    california: {
+        name: 'Terminal Island Freeway',
+        location: '',
+        duration: '',
+        type: '',
+        cost: '$30-$50 million',
+        link: 'https://www.cnu.org/what-we-do/build-great-places/terminal-island-freeway'
+    }
+}
+
+export default ref
