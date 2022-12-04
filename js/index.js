99a856ee33a186e65d217cad47e28400440f9e84
mmolta <mcmolta10@gmail.com>
2022-12-04 16:54:12 -0500

    add top arrow

--- js/index.js
+++ js/index.js
@@ -2,9 +2,21 @@ import completed from "./completed.js";
 import future from "./future.js";
 import { populateCompleted, populateFuture } from "./makeProjects.js";
 
-
+const main = document.getElementById('main')
 const completedProjects = document.getElementById('completed-projects')
 const futureProjects = document.getElementById('future-projects')
+const toTop = document.getElementById('to-top')
 
 populateCompleted(completedProjects, completed)
 populateFuture(futureProjects, future)
+
+const observer = new IntersectionObserver(el => {
+    if(el[0].isIntersecting) {
+        toTop.style.opacity = '100%'
+    }
+    else {
+        toTop.style.opacity = '0%'
+    }
+}, { threshold: [0.1] })
+
+observer.observe(main)
