0f983ad77ccadb3b2acf7d8881d34ddd157dc69f
mmolta <mcmolta10@gmail.com>
2022-12-06 17:06:06 -0500

    nav scrolling

--- js/index.js
+++ js/index.js
@@ -2,14 +2,39 @@ import completed from "./completed.js";
 import future from "./future.js";
 import { populateCompleted, populateFuture } from "./makeProjects.js";
 
+
 const main = document.getElementById('main')
 const completedProjects = document.getElementById('completed-projects')
 const futureProjects = document.getElementById('future-projects')
 const toTop = document.getElementById('to-top')
+const navBtns = document.querySelectorAll('.nav-btn')
+
+const sections = {
+    completed: null,
+    planned: null,
+    glossary: null,
+    references: null
+}
 
 populateCompleted(completedProjects, completed)
 populateFuture(futureProjects, future)
 
+// nav
+navBtns.forEach(btn => {
+    btn.onclick = e => {
+        const section = e.target.dataset.navto
+
+        if(!sections[section]) sections[section] = document.getElementById(section)
+
+        const el = sections[section]
+
+        console.log(el)
+
+        el.scrollIntoView({behavior: 'smooth'})
+    }
+})
+
+// to top jawn
 const observer = new IntersectionObserver(el => {
     if(el[0].isIntersecting) {
         toTop.style.opacity = '100%'
