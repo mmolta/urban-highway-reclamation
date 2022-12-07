6d7b40ab7b783466c9511c3e5225ea4a500a2e8b
mmolta <mcmolta10@gmail.com>
2022-12-07 01:50:09 -0500

    render basic map

--- js/index.js
+++ js/index.js
@@ -19,3 +19,29 @@ navBtns.forEach(btn => btn.onclick = e => clickNav(e))
 const observer = clickToTop(toTop)
 
 observer.observe(main)
+
+
+// @TEST MAP
+mapboxgl.accessToken = 'pk.eyJ1IjoibW1vbHRhIiwiYSI6ImNqZDBkMDZhYjJ6YzczNHJ4cno5eTcydnMifQ.RJNJ7s7hBfrJITOBZBdcOA'
+
+const initMap = () => {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [-96, 37.8],
+        zoom: 3.75
+    })
+
+    const navigationControl = new mapboxgl.NavigationControl();
+
+    map.addControl(navigationControl)
+
+    return map
+}
+
+// @MAP TODOS:
+    // - only render USA
+    // much simpler style (render as little info as possible)
+    // disable scrolling / other interactions
+
+// initMap()
