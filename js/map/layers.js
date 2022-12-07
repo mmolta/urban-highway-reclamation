8c013fa5df011906f48795f53e91aef53af1316f
mmolta <mcmolta10@gmail.com>
2022-12-07 10:31:41 -0500

    legend

--- js/map/layers.js
+++ js/map/layers.js
@@ -5,20 +5,15 @@ const projectsLayer = {
     source: 'projects',
     'source-layer': 'projects-3w0wjb',
     paint: {
-        "circle-radius": 5,
+        "circle-radius": 8,
         "circle-color": ["match",
             ["get", "type"],
             "completed", "#1D7874",
             "planned", "#EFCB68",
             "#fff"
         ],
-        "circle-stroke-width": 1.25,
-        "circle-stroke-color": ["match",
-            ["get", "type"],
-            "completed", "#4B7F52",
-            "planned", "#FDFFFC",
-            "#fff"
-        ],
+        "circle-stroke-width": 1,
+        "circle-stroke-color": "#FDFFFC",
         "circle-stroke-opacity": 0.9,
     }
 }
