e061cd7d40e5701892c39174ee40518f3927ee61
mmolta <mcmolta10@gmail.com>
2022-12-07 14:30:32 -0500

    begin setting up hoverList

--- js/index.js
+++ js/index.js
@@ -1,22 +1,16 @@
-import completed from "./completed.js";
-import future from "./future.js";
 import { clickNav, clickToTop } from "./home.js";
-import { populateCompleted, populateFuture } from "./makeProjects.js";
 import initMap from "./map/map.js";
 import projectsLayer from './map/layers.js'
 import sources from './map/sources.js'
+import { hoverProject, unHoverProject } from "./map/mapEvents.js";
 
 const main = document.getElementById('main')
-const completedProjects = document.getElementById('completed-projects')
-const futureProjects = document.getElementById('future-projects')
 const toTop = document.getElementById('to-top')
 const navBtns = document.querySelectorAll('.nav-btn')
+const mapList = document.getElementById('map-list')
 
+// default state
 localStorage.setItem('project-hovered', '')
-
-// default content
-populateCompleted(completedProjects, completed)
-populateFuture(futureProjects, future)
 
 // default interactions
 navBtns.forEach(btn => btn.onclick = e => clickNav(e))
@@ -28,67 +22,47 @@ observer.observe(main)
 //     - only render USA (?)
 //     stripped down basemap (?)
 
-const map = initMap()
+// const map = initMap()
 
-map.on('load', () => {
-    map.addSource('projects', sources.projects)
-    map.addLayer(projectsLayer)
+// map.on('load', () => {
+//     map.addSource('projects', sources.projects)
+//     map.addLayer(projectsLayer)
 
-    map.on('mouseenter', 'project-circles', e => hoverProject(e))
-    map.on('mouseleave', 'project-circles', () => unHoverProject())
 
-    // @TODO: apply hover and unhover state to list items
-})
+//     map.on('mouseenter', 'project-circles', e => hoverProject(e, map))
+//     map.on('mouseleave', 'project-circles', () => unHoverProject(map))
 
-// hover state applies to:
-    // hovering on list items
-    // hovering on project circles
-const hoverProject = e  => {
-    let hoveredId = localStorage.getItem('project-hovered')
-    
-    map.getCanvas().style.cursor = 'pointer'
+mapList.onmouseover = e => {
+    let listItem;
 
-    if (e.features.length > 0) {
-        
-        // check and remove old hover state
-        if (hoveredId.length) {
-            map.setFeatureState(
-                { source: 'projects', sourceLayer: 'projects-3w0wjb', id: hoveredId },
-                { hover: false }
-            );
-        }
+    switch(e.target.nodeName) {
+        case 'STRONG':
+            listItem = e.target.parentElement
+        case 'SMALL':
+            listItem = e.target.parentElement
+            break
+        case 'UL':
+            return
+        default:
+            listItem = e.target
+    }
 
-        hoveredId = e.features[0].id;
-
-        console.log('what the fuck ', e.features[0])
-
-        // add new hover state
-        map.setFeatureState(
-            { source: 'projects', sourceLayer: 'projects-3w0wjb', id: hoveredId },
-            { hover: true }
-        );
-
-        localStorage.setItem('project-hovered', hoveredId)
+    // need a no-maintenance way to get corresponding elements
+    const shimE = {
+        features: [
+            {
+                id: e.dataCircleId
+            }
+        ]
     }
 }
 
-const unHoverProject = () => {
-    let hoveredId = localStorage.getItem('project-hovered')
-
-    map.getCanvas().style.cursor = '';
-
-    if (hoveredId.length) {
-        map.setFeatureState(
-            { source: 'projects', sourceLayer: 'projects-3w0wjb', id: hoveredId },
-            { hover: false }
-        );
-    }
-
-    localStorage.setItem('project-hovered', null)
-}
+//     // @TODO: apply hover and unhover state to list items
+//     map.on('click', 'project-circles', e => clickProjectCircle(e))
+// })
 
 // click state applies to:
     // clicking on project circles
-const clickProject = e => {
+const clickProjectCircle = e => {
 
 }
---
+++ js/map/mapEvents.js
@@ -0,0 +1,46 @@
+// hover state applies to:
+    // hovering on list items
+    // hovering on project circles
+const hoverProject = (e, map)  => {
+    let hoveredId = localStorage.getItem('project-hovered')
+    
+    map.getCanvas().style.cursor = 'pointer'
+
+    if (e.features.length > 0) {
+        
+        // check and remove old hover state
+        if (hoveredId.length) {
+            map.setFeatureState(
+                { source: 'projects', sourceLayer: 'projects-3w0wjb', id: hoveredId },
+                { hover: false }
+            );
+        }
+
+        hoveredId = e.features[0].id;
+
+        // add new hover state
+        map.setFeatureState(
+            { source: 'projects', sourceLayer: 'projects-3w0wjb', id: hoveredId },
+            { hover: true }
+        );
+
+        localStorage.setItem('project-hovered', hoveredId)
+    }
+}
+
+const unHoverProject = map => {
+    let hoveredId = localStorage.getItem('project-hovered')
+
+    map.getCanvas().style.cursor = '';
+
+    if (hoveredId.length) {
+        map.setFeatureState(
+            { source: 'projects', sourceLayer: 'projects-3w0wjb', id: hoveredId },
+            { hover: false }
+        );
+    }
+
+    localStorage.setItem('project-hovered', null)
+}
+
+export { hoverProject, unHoverProject }
